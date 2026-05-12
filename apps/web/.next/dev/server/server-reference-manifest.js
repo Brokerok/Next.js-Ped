f5fc69ab3d66d46b9727ac25bf19a1811bb347c9
Pavlo Shev4uk <95368807+Brokerok@users.noreply.github.com>
2026-05-12 03:14:35 +0300

    fix: remove .next cache from git, fix .gitignore for monorepo

--- apps/web/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ueK1UEHous5XylH1ovO+/DvHPUjnczAAB6qgHZgDX2k=\"\n}"
--- apps/web/.next/dev/static/chunks/apps_web_src_app_page_tsx_0q7osd.._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0kthhx-._.js","static/chunks/apps_web_src_app_components_Counter_tsx_03iw7qr._.js"],
-    source: "dynamic"
-});
